bfc87b7484c33e1934d43e48f8732dc4932bfbef
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-12-08 15:42:14 +0200

    Set up basic server configs

--- server.js
+++ server.js
@@ -17,10 +17,12 @@ app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-  
-}
+   app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+   });
+  }
