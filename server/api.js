96b91eaab32966f712ea9def5169767d05fe6507
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-12-09 15:00:35 +0200

    Add ideas router

--- server/api.js
+++ server/api.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const apiRouter = express.Router();
 const minionsRouter = require('./routes/minions');
+const ideasRouter = require('./routes/ideas');
 const {} = require('./db')
 
 apiRouter.use('/minions', minionsRouter);
-
+apiRouter.use('/ideas', ideasRouter);
 
 module.exports = apiRouter;
---
+++ server/routes/ideas.js
@@ -0,0 +1,52 @@
+const ideasRouter = require('express').Router();
+const {
+    createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+  } = require('../db');
+
+module.exports = ideasRouter;
+
+ideasRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('ideas'));
+})
+
+ideasRouter.param('ideaId', (req, res, next, ideaId) => {
+    let idea = getFromDatabaseById('ideas', ideaId);
+    if (idea){
+        req.idea = idea;
+        next();
+    }
+    else {
+        res.status(404).send('Idea not found');
+    }
+})
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+    res.send(req.idea);
+});
+
+ideasRouter.post('/', (req, res, next) => {
+    let idea = addToDatabase('ideas', req.body);
+    res.status(201).send(idea);
+})
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+    let idea = updateInstanceInDatabase('ideas', req.body);
+    res.send(idea);
+})
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+    let idea = deleteFromDatabasebyId('ideas', req.params.ideaId);
+    if (idea) {
+        res.status(204).send('Idea deleted!');
+
+    }
+    else{
+        res.status(404).send('Idea not found');
+    }
+})
